89c62684428fb6b1717b06e80f020fbe238883bd
brais421520 <braisravina@rocketmail.com>
2020-02-13 15:27:19 +0000

    Update Datadownload.js

---
+++ AirBDN/react-frontend/src/components/Datadownload/Datadownload.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Datadownload.css";
+
+export default function Newsletter({ darkMode }) {
+    // css objects
+    const baseStyle = {
+      marginTop: "60px",
+      // width: "80%",
+      // height: "80%",
+      // marginLeft: "10%",
+      textAlign: "center",
+      fontSize: "2em"
+      // marginLeft: "15%",
+      // width: "70%",
+      // marginTop: "60px"
+    };
+}

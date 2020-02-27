b7d985af974c50d08522889599302745e6d9a260
kalin104 <57560372+kalin104@users.noreply.github.com>
2020-02-27 10:38:05 +0000

    Update kalin.py

--- AirBDN/weather_data/practice/spiders/kalin.py
+++ AirBDN/weather_data/practice/spiders/kalin.py
@@ -1,1 +1,20 @@
-d
+import scrapy
+
+
+class KalinSpider(scrapy.Spider):
+    name = 'kalin'
+    allowed_domains = [
+        'https://www.timeanddate.com/weather/uk/aberdeen/historic?month=5&year=2016']
+    start_urls = [
+        'http://www.timeanddate.com/weather/uk/aberdeen/historic?month=5&year=2016']
+
+    def parse(self, response):
+        dic = {}
+        for i in range(1, 44):
+            dic['time'] = response.xpath(
+                '//*[@id="wt-his"]/tbody/tr[' + str(i) + ']/th/text()').extract()
+            dic['humidity'] = response.xpath(
+                '//*[@id="wt-his"]/tbody/tr[' + str(i) + ']/td[6]/text()').extract()
+            print(dic)
+            yield dic
+     

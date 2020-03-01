39b6120c843cb6eb6bd7172e6161bbc1c2552c09
agueeros10 <alex.agueros@gmail.com>
2020-03-01 21:21:59 +0000

    Update about.js
    Giving a structure and seeing how it looks, "." will be exchanged with text when ready

--- react-frontend/src/components/About/About.js
+++ react-frontend/src/components/About/About.js
@@ -5,6 +5,12 @@ export default function About() {
 	return (
 		<div className='aboutPage'>
 			<h1>About Us</h1>
+			<h2>Who we are:</h2>
+			<p>.</p>
+			<h3>The page(?)</h3>
+			<p>.</p>
+			<h4>Our choices</h4>
+			<p>.</p>
 		</div>
 	)
 }

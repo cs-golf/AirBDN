11716d104d7e8c1f1636f4bc190b5cc4c262c0db
kalin104 <57560372+kalin104@users.noreply.github.com>
2020-04-19 17:54:47 +0100

    Update req.py

--- req.py
+++ req.py
@@ -1,6 +1,7 @@
 # exctracting data from https://www.timeanddate.com/weather/uk/aberdeen/
 import requests
 import re
+import datetime
 import typing
 from bs4 import BeautifulSoup as soup
 import contextlib
@@ -8,21 +9,23 @@ from selenium import webdriver
 import time
 from pymongo import MongoClient
 
+# mongo initialize
 client = MongoClient('localhost', 27017)
 db = client['test-database']
 collection = db['test-collection']
 
-# extracting the table off the page
+# scraping tool for historic data of Aberdeen
 
 
 class Scrape:
-    def __init__(self, country, city, yearStart, yearEnd):
+    # initialize
+    def __init__(self, country, city, yearStart):
         self.city = city
         self.yearStart = yearStart
-        self.yearEnd = yearEnd
         self.driver = webdriver.Chrome('E:/chromedriver.exe')
         self.country = country
 
+    # scrape the HTML table of a page
     def _remove(self, d: list) -> list:
         return list(filter(None, [re.sub('\xa0', '', b) for b in d]))
 
@@ -36,35 +39,42 @@ class Scrape:
         _h2 = self._remove(h2)
         yield {tuple(self._remove(h1)): [dict(zip(_h2, self._remove([a, *i]))) for [[a], *i] in data]}
 
+    # visit each page up to date to scrape
+    # controlling years & month with the url
+    # while using selenium for JS in order to select a date
+
     def execute(self):
-        # year , month change by changing url
-        for year in range(self.yearStart, self.yearEnd):
-            for month in range(1, 12):
-                try:
-                    data_collection = {}
-                    self.driver.get('https://www.timeanddate.com/weather/'+self.country+'/'+self.city+'/historic?month=' +
-                                    str(month) + '&year='+str(year))
-                # changing js button selection
-                    for i in self.driver.find_element_by_id('wt-his-select').find_elements_by_tag_name('option'):
-                        i.click()
-                        with self.get_weather_data(self.driver.page_source, False) as weather:
-                            data_collection[i.text] = weather
-                    for date, raw_data in data_collection.items():
-                        for data in raw_data[('Conditions', 'Comfort')]:
-                            data['Date'] = date
-                            data['Humidity'] = data['Barometer']
-                            data['Barometer'] = data['Visibility']
-                            del data['Visibility']
-                            print(data)
-                            collection.insert_one(data)
-                            # here push data in mongo db
-                            # data is a small json format element to push in
-                except:
-                    print("An exception occurred")
+        now = datetime.datetime.now()
+        for year in range(self.yearStart, now.year+1):
+            for month in range(1, 13):
+                if not ((year == now.year) and (now.month < month)):
+                    try:
+                        data_collection = {}
+                        self.driver.get('https://www.timeanddate.com/weather/'+self.country+'/'+self.city+'/historic?month=' +
+                                        str(month) + '&year='+str(year))
+
+                        # changing js button selection to iterate though the dates
+                        for i in self.driver.find_element_by_id('wt-his-select').find_elements_by_tag_name('option'):
+                            i.click()
+                            with self.get_weather_data(self.driver.page_source, False) as weather:
+                                data_collection[i.text] = weather
+
+                        # restructure the taken json
+                        for date, raw_data in data_collection.items():
+                            for data in raw_data[('Conditions', 'Comfort')]:
+                                data['Date'] = date
+                                data['Humidity'] = data['Barometer']
+                                data['Barometer'] = data['Visibility']
+                                del data['Visibility']
+                                print(data)
+                                collection.insert_one(data)
+                                # here push data in mongo db
+                                # data is a small json format element to push in
+                    except:
+                        print("An exception occurred")
+        print("work finished")
 
 
 # name city  years between scraping for dateandtime.com
-p1 = Scrape('uk', 'aberdeen', 2014, 2020)
+p1 = Scrape('uk', 'aberdeen', 2019)
 p1.execute()
-
-

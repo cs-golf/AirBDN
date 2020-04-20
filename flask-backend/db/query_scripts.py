978c4a6a5c01f4f38dcc2f05f5b9c82c3d6fb1e3
agueeros10 <alex.agueros@gmail.com>
2020-04-20 01:54:02 +0100

    Revert "o"
    This reverts commit b16a29da3046e7b2f9e8efff7c50746f3aae469d.

--- flask-backend/db/query_scripts.py
+++ flask-backend/db/query_scripts.py
@@ -20,25 +20,3 @@ def floor_date(date, period_in_s=300):
     ts = datetime.timestamp(date)
     new_ts = ts - (ts % 300)
     return(datetime.fromtimestamp(new_ts))
-<<<<<<< Updated upstream:flask-backend/db/query_scripts.py
-=======
-
-
-
-def compare_times(time_a, time_b, time_dif=10):
-    time_a = parse(time_a)
-    time_b = parse(time_b)
-    if time_a >= time_b:
-        difference = time_a-time_b
-    elif time_a < time_b:
-        difference = time_b-time_a
-
-    if difference.seconds < time_dif:
-        return True
-
-
-# ----test----
-# t1 ="2020-03-30 22:00:00"
-# t2 = "2020-03-30 22:00:06"
-# compare_times( t2,t1)
->>>>>>> Stashed changes:flask-backend/query_scripts.py
